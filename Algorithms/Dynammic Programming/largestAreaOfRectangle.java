03989f0e99c2b5dc68fc77b6cdbfb2ab3cdf6c91
YashnitKalra <gauravn67@gma.com>
2021-07-25 14:48:41 +0530

    Updated

---
+++ Algorithms/Dynammic Programming/largestAreaOfRectangle.java
@@ -0,0 +1,45 @@
+// calculate maximum histogram each row
+// 1. If value is 1 increase the value in hist array
+// 2. If value is 0 update the value to 0 in hist array
+
+// maximum histogram area
+// 1. Add index to stack if current value is bigger than top of stack
+// 2. Otherwise keep removing from stack till a number which is smaller or equal than current is found.
+// 3. Calculate area everytime you pop -> area from [top index, current index)
+
+import java.util.*;
+
+class largestAreaOfRectangle{
+
+    public static int getMaxHistogramArea(int[] arr){
+        int maxArea = 0;
+        Stack<Integer> st = new Stack<>();
+        int i = 0;
+        while(i < arr.length){
+            while(!st.empty() && arr[st.peek()] >= arr[i]){
+                maxArea = Math.max(maxArea, arr[st.peek()] * (i-st.pop()));
+            }
+            st.add(i++);
+        }
+        while(!st.empty())
+            maxArea = Math.max(maxArea, arr[st.pop()] * (st.empty()?i:(i-st.peek()-1)) );
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{
+            {1,0,0,1,1,1},
+            {1,0,1,1,0,1},
+            {0,1,1,1,1,1},
+            {0,1,1,1,1,1}
+        };
+        int hist[] = new int[arr[0].length];
+        int maxArea = 0;
+        for(int[] row: arr){
+            for(int i=0; i<row.length; i++)
+                hist[i] = (row[i]==0 ? 0 : hist[i]+1);
+            maxArea = Math.max(maxArea, getMaxHistogramArea(hist));
+        }
+        System.out.println(maxArea);
+    }
+}
